04a406d18cacc1ac687fe34d805593646e4c29c3
Paul Brodersen <paulbrodersen@gmail.com>
2026-04-10 15:26:52 +0100

    Draw ellipses to show standard deviations

--- vizout2/main.py
+++ vizout2/main.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from matplotlib.patches import Ellipse
+from matplotlib.transforms import Affine2D
+
 
 class ClickableArtists:
     """Implements selection of matplotlib artists via the mouse left click (+/- ctrl or command key).
@@ -242,7 +245,7 @@ class SelectableArtistGroups(SelectableAnnotatedArtists):
 
 class OutlierSelector:
 
-    def __init__(self, data, markersize=0.05, **kwargs):
+    def __init__(self, data, markersize=0.05, n_std=2, **kwargs):
 
         total_points, total_columns = data.shape
         figure, axes = plt.subplots(
@@ -253,6 +256,7 @@ class OutlierSelector:
         )
 
         self.artist_groups = list(self._draw(data, axes, markersize, **kwargs))
+        self._draw_standard_deviations(data, axes, n_std)
         self._label_axes(data, axes)
         self._link_peers(data)
         figure.tight_layout()
@@ -283,6 +287,22 @@ class OutlierSelector:
         return artists
 
 
+    def _draw_standard_deviations(self, data, axes, n_std):
+        for ii, x in enumerate(data.columns):
+            for jj, y in enumerate(data.columns):
+                if ii > jj:
+                    confidence_ellipse(
+                        data[x], data[y], axes[ii, jj], n_std=n_std,
+                        edgecolor="red", linewidth=1)
+
+
+    def _label_axes(self, data, axes):
+        for ax, label in zip(axes[:, 0], data.columns):
+            ax.set_ylabel(label)
+        for ax, label in zip(axes[0, :], data.columns):
+            ax.set_title(label)
+
+
     def _link_peers(self, data):
         """For every data-row index, collect the corresponding artist from each
         subplot and register all of them as peers of one another."""
@@ -303,11 +323,62 @@ class OutlierSelector:
                 ]
 
 
-    def _label_axes(self, data, axes):
-        for ax, label in zip(axes[:, 0], data.columns):
-            ax.set_ylabel(label)
-        for ax, label in zip(axes[0, :], data.columns):
-            ax.set_title(label)
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The Axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+
+    Notes
+    -----
+    From: https://matplotlib.org/stable/gallery/statistics/confidence_ellipse.html
+
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensional dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                          facecolor=facecolor, **kwargs)
+
+    # Calculating the standard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the standard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
 
 
 if __name__ == "__main__":
